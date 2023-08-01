f3a2b8201c39f5ac021ddd40e430f7b9b3562bae
Victor Ferraz <vferraz48@gmail.com>
2023-08-01 14:38:39 -0300

    Add files via upload

---
+++ Day 6/Aula06-T2/Aula06-T2/Aula06_T2App.swift
@@ -0,0 +1,17 @@
+//
+//  Aula06_T2App.swift
+//  Aula06-T2
+//
+//  Created by Student on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Aula06_T2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
